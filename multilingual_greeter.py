1a954aefc0c45f7e1d629393e2a91405a79f255b
Joey8419 <jnelson297@gmail.com>
2023-11-08 06:34:40 -0500

    finished

--- multilingual_greeter.py
+++ multilingual_greeter.py
@@ -13,31 +13,33 @@ greet(name_input())
 
 def language_input():
     print("Choose one of these lanuages")
-    print ("1. Creole")
+    print ("1. Creole/ French")
     print ("2. English")
     print("3. Spanish")
 
-while True:
-    try:
-        usersChoice = int(input("Enter your number choice"))
-        if choice == 1:
-            chosen_language = "Creole"
-        elif choice == 2:
+    choice_of_language = input("Enter your number choice of the language you prefer")
+
+    if choice_of_language == '1':
+            chosen_language = "Creole/ French"
+            name = input("Indiquez votre nom ")  
+            print("Bonjour, " + name + "!")
+    elif choice_of_language == '2':
             chosen_language = "English"
-        elif choice == 3:
-            chosen_language == "Spanigh"
-        else:
-            print("Invalid input")
+            name = input("Provide your name ")
+            print("Hello " + name + "!")
+    elif choice_of_language == '3':
+            chosen_language = "Spanish"
+            name = input("Proporciona tu nombre")
+            print("Hola " + name + "!")
+    else:
+            print("Invalid input. Please choose number 1, 2, or 3")
+    
+# Call the language_input() function
+while True:
+      language_input()
+      another_choice = input("Do you want to choose another language? (yes/no)")
+      if another_choice != 'yes':
             break
-
-        if choice == 1:
-            print("Bonjour, Salut comment tu t'appelles")
-        elif choice == 2:
-            print("Hello, what is your name?")
-        elif choice ==3:
-            print("Hola Cuál es su nombre")
-
-
 
 
 
